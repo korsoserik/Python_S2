1b18b3eeeb2dd1ff0bab6fa57062015d714e9082
korsoserik <erik.korsos@gmail.com>
2021-05-09 12:35:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+a = int(input("Adja meg az a oldal hosszúságát méterben: "))
+kerulet = a*4
+terulet = a*a
+print("A Négyzet területe:", terulet, ("m2"))
+print("A Négyzet kerulete:", kerulet, ("m"))
